c39e113584954c17a6b7f078e8e5021384e93ab0
1100jimjim <161785835+1100jimjim@users.noreply.github.com>
2024-06-07 16:13:55 +0800

    Update min.js

--- 作業-4/min.js
+++ 作業-4/min.js
@@ -1,12 +1,9 @@
-function arraymin(a){
-    let min = a[0]; 
-    
-    for (let i = 1; i < a.length; i++) {
-        if (a[i] < min) {
-            min = a[i]; 
-        }
-    }
-    return min
-}
-
-console.log(arraymin([3, 9, 2, 6, 1]));
+function min(a,b) {
+    if (a>b)
+      return b;
+    else
+      return a;
+  }
+ 
+  var m = min(9,2);
+  console.log("min(9,2)=",m);
